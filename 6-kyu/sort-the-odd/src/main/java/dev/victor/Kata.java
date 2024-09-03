efe251b62a22405b56a601f11b636044007425e4
Victor Elgersma <victor.elgersma41@thg.com>
2024-09-03 08:36:16 +0100

    refactored sort the odd

--- 6-kyu/sort-the-odd/src/main/java/dev/victor/Kata.java
+++ 6-kyu/sort-the-odd/src/main/java/dev/victor/Kata.java
@@ -1,35 +1,24 @@
 package dev.victor;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Kata {
 
     public static int[] sortArray(int[] arr) {
 
-        // create new array with just the odds
-        ArrayList<Integer> oddsArray = new ArrayList<>();
+        // Find all the odd numbers and sort them
+        int[] sortedOdds = Arrays.stream(arr).filter(num -> num % 2 != 0).sorted().toArray();
 
-        for (int j : arr) {
-            if (j % 2 != 0) {
-                oddsArray.add(j);
+        // Reconstruct the sorted array by placing the sorted odd numbers back into their positions
+
+        int oddIndex = 0;
+        for (int i = 0; i< arr.length; i++) {
+            if (arr[i] %2 != 0) {
+                arr[i] = sortedOdds[oddIndex++];
             }
         }
-        // sort this array in place
-        oddsArray.sort(Integer::compareTo);
 
-        // then combine the original array with the new array - whenever we encounter an odd number replace it with the ith number of our new array
-        
-        ArrayList<Integer> arrayWithOddsSorted = new ArrayList<>();
+        return arr;
 
-        int c = 0;
-        for (int k : arr) {
-            if (k % 2 == 0) {
-                arrayWithOddsSorted.add(k);
-            } else {
-                arrayWithOddsSorted.add(oddsArray.get(c));
-                c++;
-            }
-        }
-        return arrayWithOddsSorted.stream().mapToInt(i -> i).toArray();
     }
 }
